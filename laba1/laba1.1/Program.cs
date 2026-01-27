450427487db1fc169be1ac9bde3885210c24cdc9
Yauheni Charniauski <chernyavskij.z@mail.ru>
2026-01-27 18:47:19 +0300

    Add laba1.2

--- laba1/laba1.1/Program.cs
+++ laba1/laba1.1/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(opt =>
+    opt.AddDefaultPolicy(policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    }));
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapGet("/", () => "Yauheni");
 
