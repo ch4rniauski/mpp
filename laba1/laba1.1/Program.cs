290190e569011e54016761098288cd129f5fb78a
Yauheni Charniauski <chernyavskij.z@mail.ru>
2026-01-28 16:28:20 +0300

    Complete laba1

--- laba1/laba1.1/Program.cs
+++ laba1/laba1.1/Program.cs
@@ -13,6 +13,20 @@ var app = builder.Build();
 
 app.UseCors();
 
-app.MapGet("/", () => "Yauheni");
+app.MapGet("/", () => new List<ClientDto>
+{
+    new(Guid.NewGuid(), "person", "Иванов Иван", "AB1234567", "Минск", "+375291111111"),
+    new(Guid.NewGuid(), "company", "Рога-Копыта", null, "Гродно", "+375152444444"),
+    new(Guid.NewGuid(), "person", "Петров Петр", "AB7654321", "Минск", "+375292222222")
+});
 
 await app.RunAsync();
+
+internal record ClientDto(
+    Guid Id,
+    string ClientType,
+    string Name,
+    string? Passport,
+    string Address,
+    string Phone
+);
